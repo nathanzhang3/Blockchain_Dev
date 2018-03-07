0f53cf6c25b3b219fc2367319a05fc2296355f47
Nathan Zhang <nathanzhang3@gmail.com>
2018-03-07 02:03:40 -0600

    Create new block: need to create the first block (the one without previous block) with proof of work

--- blockchain_test.py
+++ blockchain_test.py
@@ -1,11 +1,39 @@
+# https://learnblockchain.cn/2017/10/27/build_blockchain_by_python/
+
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
+        # Create the genesis Block
+        self.new_block(previous_hash=1, proof=100)
+
     def new_block(self):
         # Creates a new Block and adds it to the chain
-        pass
+        """
+        生成新块
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Blockchain
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp':time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+
+        # Reset the current list of current_transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+        }
 
     def new_transaction(self):
         # Adds a new transaction to the list of transactions
@@ -28,9 +56,18 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         # Hashes a Block
-        pass
+        """
+        生成块的 SHA-256 hash值
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is ordered, or we'll have
+        # inconssitent Hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
